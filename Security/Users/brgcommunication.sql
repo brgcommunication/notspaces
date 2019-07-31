a4dcf8c8f5f136e91694aba1d84e02f5cce9bd90
Luca Battistini <lucabattistini@brg.it>
2019-07-31 16:46:51 +0200

    Revert "[DB] Initial commit"
    This reverts commit ea73083247438e781c3e740bf6672872e34bc4f5.

--- Security/Users/brgcommunication.sql
+++
@@ -1,5 +0,0 @@
-IF NOT EXISTS (SELECT * FROM master.dbo.syslogins WHERE loginname = N'brgcommunication')
-CREATE LOGIN [brgcommunication] WITH PASSWORD = 'p@ssw0rd'
-GO
-CREATE USER [brgcommunication] FOR LOGIN [brgcommunication]
-GO
